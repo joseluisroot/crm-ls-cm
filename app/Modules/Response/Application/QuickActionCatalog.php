1fb9165da24b306aec5cf5e0d4af8cffef17cbb8
José Luis Reyes <joseluis.r85@gmail.com>
2026-07-14 04:28:24 -0600

    feat(response): add political care quick actions

--- app/Modules/Response/Application/QuickActionCatalog.php
+++ app/Modules/Response/Application/QuickActionCatalog.php
@@ -9,6 +9,11 @@ final class QuickActionCatalog
     public function all(): array
     {
         return [
+            ['command' => '/servir', 'label' => 'Estamos para servir', 'body' => 'Hola {nombre}, muchas gracias por sus palabras. Estamos para servirle.'],
+            ['command' => '/contactar', 'label' => 'Equipo contactará', 'body' => 'Hola {nombre}, gracias por escribirnos. Nuestro equipo estará contactándote para dar seguimiento a tu caso y solicitar la información necesaria.'],
+            ['command' => '/escribenos', 'label' => 'Solicitar mensaje privado', 'body' => 'Hola {nombre}, gracias por escribirnos. Para poder darte seguimiento y proteger tus datos, por favor envíanos un mensaje privado.'],
+            ['command' => '/retomar', 'label' => 'Retomar seguimiento', 'body' => 'Hola {nombre}, queremos continuar dando seguimiento a tu atención. ¿Podrías compartirnos la información solicitada?'],
+            ['command' => '/ayuda-flujo', 'label' => 'Ayuda con el flujo', 'body' => 'Hola {nombre}, estamos para ayudarte. Cuéntanos brevemente qué necesitas y un miembro de nuestro equipo dará seguimiento.'],
             ['command' => '/recibido', 'label' => 'Solicitud recibida', 'body' => 'Hola {nombre}, gracias por escribirnos. Hemos recibido tu mensaje y ya estamos revisando la información para darte seguimiento.'],
             ['command' => '/agradecer', 'label' => 'Agradecimiento', 'body' => 'Hola {nombre}, muchas gracias por comunicarte y compartirnos tu comentario. Valoramos mucho tu participación.'],
             ['command' => '/seguimiento', 'label' => 'En seguimiento', 'body' => 'Hola {nombre}, queremos informarte que tu solicitud continúa en seguimiento. Te compartiremos una actualización tan pronto tengamos más información.'],
