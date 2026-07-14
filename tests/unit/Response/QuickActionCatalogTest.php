e2d46c093d611dbb00791b38ca8e6107c5f196e3
José Luis Reyes <joseluis.r85@gmail.com>
2026-07-14 04:38:57 -0600

    test: cover citizen care response intents

--- tests/unit/Response/QuickActionCatalogTest.php
+++ tests/unit/Response/QuickActionCatalogTest.php
@@ -9,15 +9,23 @@ use PHPUnit\Framework\TestCase;
 
 final class QuickActionCatalogTest extends TestCase
 {
-    public function testCatalogProvidesPersonalizedCommands(): void
+    public function testCatalogProvidesPublicAndPrivateCitizenCareCommands(): void
     {
         $catalog = new QuickActionCatalog();
         $actions = $catalog->all();
+        $commands = array_column($actions, 'command');
+        $intents = array_column($actions, 'intent');
 
-        self::assertNotEmpty($actions);
-        self::assertSame('/recibido', $actions[0]['command']);
+        self::assertContains('/servir', $commands);
+        self::assertContains('/contactar', $commands);
+        self::assertContains('/escribenos', $commands);
+        self::assertContains('/retomar', $commands);
+        self::assertContains('/ayuda-flujo', $commands);
+        self::assertContains('PUBLIC', $intents);
+        self::assertContains('PRIVATE', $intents);
 
-        $personalized = $catalog->personalize($actions[0], 'Juan Pérez');
+        $contactAction = $actions[array_search('/contactar', $commands, true)];
+        $personalized = $catalog->personalize($contactAction, 'Juan Pérez');
         self::assertStringContainsString('Juan Pérez', $personalized['body']);
         self::assertStringNotContainsString('{nombre}', $personalized['body']);
     }
